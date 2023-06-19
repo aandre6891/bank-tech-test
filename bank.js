bb63a8a782492f27854e569bc2ce7491bc2d6df7
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-06-19 16:13:47 +0100

    removed example inputs

--- bank.js
+++ bank.js
@@ -53,9 +53,3 @@ class Bank {
 }
 
 module.exports = Bank;
-
-const bank = new Bank();
-bank.deposit(1000, "10/01/2023");
-bank.deposit(2000, "13/01/2023");
-bank.withdraw(500, "14/01/2023");
-bank.printStatement();
