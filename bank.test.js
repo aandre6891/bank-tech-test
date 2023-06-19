14c084000d05983522663da4359a39f0fdc3f879
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-06-19 15:05:40 +0100

    5 passing test

--- bank.test.js
+++ bank.test.js
@@ -8,13 +8,33 @@ describe("Bank", () => {
 
   test("making a deposit of 1000 it should add the amount to the account", () => {
     const bank = new Bank();
-    bank.deposit(1000, "10-01-2023");
-    expect(bank.account).toEqual([{ amount: 1000, date: "10-01-2023", type: "deposit" }]);
+    bank.deposit(1000, "10/01/2023");
+    expect(bank.account).toEqual([
+      { amount: 1000, date: "10/01/2023", type: "deposit", balance: 1000 },
+    ]);
   });
 
   test("making a withdrawal of 500 it should add the operation to the account", () => {
     const bank = new Bank();
-    bank.withdraw(500, "14-01-2023");
-    expect(bank.account).toEqual([{ amount: 500, date: "14-01-2023", type: "withdrawal" }]);
-  })
+    bank.withdraw(500, "14/01/2023");
+    expect(bank.account).toEqual([
+      { amount: 500, date: "14/01/2023", type: "withdrawal", balance: -500 },
+    ]);
+  });
+
+  test("making a deposit of 1000 it should calculate a balance of 1000", () => {
+    const bank = new Bank();
+    bank.deposit(1000, "10/01/2023");
+    const lastOperation = bank.account[bank.account.length - 1];
+    expect(lastOperation.balance).toEqual(1000);
+  });
+
+  test("after multiple operations it should return a balance of 2500", () => {
+    const bank = new Bank();
+    bank.deposit(1000, "10/01/2023");
+    bank.deposit(2000, "13/01/2023");
+    bank.withdraw(500, "14/01/2023");
+    const lastOperation = bank.account[bank.account.length - 1];
+    expect(lastOperation.balance).toEqual(2500);
+  });
 });
