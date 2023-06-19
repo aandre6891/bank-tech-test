b67dc398bdcaf0df30f53c294047333aafc42dcd
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-06-19 15:07:31 +0100

    completed #calculateBalance and amended #withdraw to add the newBalance to the array

--- bank.js
+++ bank.js
@@ -4,19 +4,40 @@ class Bank {
   }
 
   deposit(value, date) {
+    const newBalance = this.calculateBalance(value, "deposit");
     this.account.push({
       amount: value,
       date: date,
       type: "deposit",
+      balance: newBalance,
     });
   }
 
   withdraw(value, date) {
+    const newBalance = this.calculateBalance(value, "withdrawal");
     this.account.push({
       amount: value,
       date: date,
       type: "withdrawal",
+      balance: newBalance,
     });
+  }
+
+  calculateBalance(value, type) {
+    let previousBalance = 0;
+
+    if (this.account.length >= 1) {
+      const lastOperation = this.account[this.account.length - 1];
+      previousBalance = lastOperation.balance;
+    } else {
+      previousBalance = 0;
+    }
+
+    if (type === "deposit") {
+      return previousBalance + value;
+    } else {
+      return previousBalance - value;
+    }
   }
 }
 
