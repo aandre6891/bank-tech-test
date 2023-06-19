893caa65c1add74b1358f6fe4038f0b3139edcec
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-06-19 13:22:34 +0100

    added second test for deposit and deposit method

--- bank.js
+++ bank.js
@@ -3,7 +3,13 @@ class Bank {
     this.account = [];
   }
 
-
+  deposit(value, date) {
+    this.account.push({
+      amount: value,
+      date: date,
+      type: "deposit",
+    });
+  }
 }
 
 module.exports = Bank;
--- bank.test.js
+++ bank.test.js
@@ -5,4 +5,10 @@ describe("Bank", () => {
     const bank = new Bank();
     expect(bank.account).toEqual([]);
   });
+
+  test("making a deposit of 1000 it should add the amount to the account", () => {
+    const bank = new Bank();
+    bank.deposit(1000, "10-01-2023");
+    expect(bank.account).toEqual([{ amount: 1000, date: "10-01-2023", type: "deposit" }]);
+  });
 });
