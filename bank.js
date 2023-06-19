4d9e6e0e9d4e9703874df20f8ab3c0d1d5123823
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-06-19 16:38:37 +0100

    replaced #calculateBalance with variable balance in the constructor and amended deposit and withdraw methods

--- bank.js
+++ bank.js
@@ -1,51 +1,47 @@
 class Bank {
   constructor() {
     this.account = [];
+    this.balance = 0;
   }
 
-  deposit(value, date) {
-    const newBalance = this.calculateBalance(value, "deposit");
+  deposit(amount, date) {
+    const newBalance = this.balance += amount;
     this.account.push({
-      amount: value,
+      amount: amount,
       date: date,
       type: "deposit",
       balance: newBalance,
     });
   }
 
-  withdraw(value, date) {
-    const newBalance = this.calculateBalance(value, "withdrawal");
+  withdraw(amount, date) {
+    const newBalance = this.balance -= amount;
     this.account.push({
-      amount: value,
+      amount: amount,
       date: date,
       type: "withdrawal",
       balance: newBalance,
     });
   }
 
-  calculateBalance(value, type) {
-    let previousBalance = 0;
-    const lastOperation = this.account[this.account.length - 1];
-    this.account.length ? previousBalance = lastOperation.balance : previousBalance = 0;
-    return type === "deposit" ? previousBalance + value : previousBalance - value;
-  }
-
   printStatement() {
     console.log("date || credit || debit || balance");
-    const statement = this.account.reverse().map((operation) => {
-      if(operation.type === "withdrawal") {
-        console.log(operation.date + " || || " + operation.amount.toFixed(2) + " || " + operation.balance.toFixed(2));
+    const statement = this.account.reverse().map((transaction) => {
+      if (transaction.type === "withdrawal") {
+        console.log(transaction.date + " || || " + transaction.amount.toFixed(2) + " || " + transaction.balance.toFixed(2));
       } else {
-        console.log(operation.date + " || " + operation.amount.toFixed(2) + " || || " + operation.balance.toFixed(2));  
+        console.log(transaction.date + " || " + transaction.amount.toFixed(2) + " || || " + transaction.balance.toFixed(2));
       }
-    })
-  } 
+    });
+  }
 }
 
 module.exports = Bank;
 
 const bank = new Bank();
-bank.deposit(1000, "10/01/2023");
-bank.deposit(2000, "13/01/2023");
-bank.withdraw(500, "14/01/2023");
+
+bank.deposit(1000, '10/01/2023');
+bank.deposit(2000, '13/01/2023');
+bank.withdraw(500, '14/01/2023');
+
 bank.printStatement();
